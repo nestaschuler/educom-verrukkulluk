3f934feb6e130a31cf83c1196911c98ea9b1f95e
Nesta Schuler <nesta_schuler@outlook.com>
2026-03-02 11:50:27 +0100

    Recipe_info class #8

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once("lib/user.php");
 require_once("lib/kitchentype.php"); 
 require_once("lib/recipe.php");
 require_once("lib/ingredient.php"); 
+require_once("lib/recipe_info.php"); 
+
 
 /// INIT
 $db = new database();
@@ -14,6 +16,7 @@ $us = new user ($db->getConnection());
 $kt = new kitchentype($db->getConnection()); 
 $rp = new recipe ($db->getConnection()); 
 $in = new ingredient($db->getConnection());
+$rp_in = new recipe_info ($db->getConnection());
 
 
 /// VERWERK 
@@ -22,6 +25,7 @@ $user = $us->selectUser(1);
 $kitchentype = $kt->selectKitchentype(1); 
 $recipe = $rp->selectRecipe(1); 
 $ingredient = $in->selectIngredient(1);
+$recipe_info = $rp_in->selectRecipe_info(1, "C"); 
 
 /// RETURN
 var_dump($pd);
@@ -29,3 +33,4 @@ var_dump($us);
 var_dump($kt); 
 var_dump($rp); 
 var_dump($in); 
+var_dump($rp_in);
